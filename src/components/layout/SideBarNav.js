712da1d6bef764dd01078339774126f6839e8811
dnlbell <dnl.bell@gmail.com>
2020-01-16 14:09:43 -0800

    reorg

--- src/components/layout/SideBarNav.js
+++ src/components/layout/SideBarNav.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -17,7 +16,6 @@ function SideBarNav(props) {
         event.preventDefault();
         history.push("/Read");
         props.toggleDrawer(false);
-
     }
 
     const handlePostClick = () => (event) => {
--- src/components/theme.js
+++
@@ -1,7 +0,0 @@
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
-
-const theme = createMuiTheme({
-    spacing: 4
-});
-
-export default theme;
